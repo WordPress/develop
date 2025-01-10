bb089d1c87292aa66841bfed368a304f33c546ed
Felix Arntz <felixarntz@google.com>
2025-01-10 11:08:10 -0800

    Only load view transitions implementation if browser supports it.

--- src/wp-content/themes/twentyfifteen/js/view-transitions.js
+++ src/wp-content/themes/twentyfifteen/js/view-transitions.js
@@ -1,58 +1,61 @@
-// TODO: Check for `!! window.navigation && 'CSSViewTransitionRule' in window`.
-
-const determineTransitionType = ( oldNavigationEntry, newNavigationEntry ) => {
-	if ( ! oldNavigationEntry || ! newNavigationEntry ) {
-		return 'unknown';
-	}
-
-	const currentURL = new URL( oldNavigationEntry.url );
-	const destinationURL = new URL( newNavigationEntry.url );
-
-	const currentPathname = currentURL.pathname;
-	const destinationPathname = destinationURL.pathname;
-
-	if ( currentPathname !== destinationPathname ) {
-		// If post URLs start with a date, use that to determine "order".
-		const currentDateMatches = currentPathname.match( /^\/(\d{4})\/(\d{2})\/(\d{2})\// );
-		const destinationDateMatches = destinationPathname.match( /^\/(\d{4})\/(\d{2})\/(\d{2})\// );
-		console.log( currentPathname );
-		console.log( destinationPathname );
-		if ( currentDateMatches && destinationDateMatches ) {
-			const currentDate = new Date( parseInt( currentDateMatches[ 1 ] ), parseInt( currentDateMatches[ 2 ] ) - 1, parseInt( currentDateMatches[ 3 ] ) );
-			const destinationDate = new Date( parseInt( destinationDateMatches[ 1 ] ), parseInt( destinationDateMatches[ 2 ] ) - 1, parseInt( destinationDateMatches[ 3 ] ) );
-			if ( currentDate < destinationDate ) {
-				return 'forwards';
-			}
-			if ( currentDate > destinationDate ) {
-				return 'backwards';
-			}
+if ( !! window.navigation && 'CSSViewTransitionRule' in window ) {
+	const determineTransitionType = ( oldNavigationEntry, newNavigationEntry ) => {
+		if ( ! oldNavigationEntry || ! newNavigationEntry ) {
 			return 'unknown';
 		}
 
-		// Otherwise, check URL "hierarchy".
-		if ( destinationPathname.startsWith( currentPathname ) ) {
-			return 'forwards';
+		const currentURL = new URL( oldNavigationEntry.url );
+		const destinationURL = new URL( newNavigationEntry.url );
+
+		const currentPathname = currentURL.pathname;
+		const destinationPathname = destinationURL.pathname;
+
+		if ( currentPathname !== destinationPathname ) {
+			// If post URLs start with a date, use that to determine "order".
+			const currentDateMatches = currentPathname.match( /^\/(\d{4})\/(\d{2})\/(\d{2})\// );
+			const destinationDateMatches = destinationPathname.match( /^\/(\d{4})\/(\d{2})\/(\d{2})\// );
+			console.log( currentPathname );
+			console.log( destinationPathname );
+			if ( currentDateMatches && destinationDateMatches ) {
+				const currentDate = new Date( parseInt( currentDateMatches[ 1 ] ), parseInt( currentDateMatches[ 2 ] ) - 1, parseInt( currentDateMatches[ 3 ] ) );
+				const destinationDate = new Date( parseInt( destinationDateMatches[ 1 ] ), parseInt( destinationDateMatches[ 2 ] ) - 1, parseInt( destinationDateMatches[ 3 ] ) );
+				if ( currentDate < destinationDate ) {
+					return 'forwards';
+				}
+				if ( currentDate > destinationDate ) {
+					return 'backwards';
+				}
+				return 'unknown';
+			}
+
+			// Otherwise, check URL "hierarchy".
+			if ( destinationPathname.startsWith( currentPathname ) ) {
+				return 'forwards';
+			}
+			if ( currentPathname.startsWith( destinationPathname ) ) {
+				return 'backwards';
+			}
 		}
-		if ( currentPathname.startsWith( destinationPathname ) ) {
-			return 'backwards';
+
+		return 'unknown';
+	};
+
+	window.addEventListener( 'pageswap', async ( e ) => {
+		if ( e.viewTransition ) {
+			const transitionType = determineTransitionType( e.activation.from, e.activation.entry );
+
+			e.viewTransition.types.add( transitionType );
 		}
-	}
+	} );
 
-	return 'unknown';
-};
+	window.addEventListener( 'pagereveal', async ( e ) => {
+		if ( e.viewTransition ) {
+			const transitionType = determineTransitionType( navigation.activation.from, navigation.activation.entry );
 
-window.addEventListener( 'pageswap', async ( e ) => {
-	if ( e.viewTransition ) {
-		const transitionType = determineTransitionType( e.activation.from, e.activation.entry );
+			e.viewTransition.types.add( transitionType );
+		}
+	} );
+} else {
+	console.warn( 'View transitions not loaded as the browser is lacking support.' );
+}
 
-		e.viewTransition.types.add( transitionType );
-	}
-} );
-
-window.addEventListener( 'pagereveal', async ( e ) => {
-	if ( e.viewTransition ) {
-		const transitionType = determineTransitionType( navigation.activation.from, navigation.activation.entry );
-
-		e.viewTransition.types.add( transitionType );
-	}
-} );
