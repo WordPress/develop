a451b7ef12811b95342c2b4229d216c3eb23aa9c
Sukhendu Sekhar Guria <sukhendu.guria@rtcamp.com>
2025-01-13 01:22:27 +0530

    Optimize the total count queries in REST API

--- src/wp-includes/rest-api/endpoints/class-wp-rest-users-controller.php
+++ src/wp-includes/rest-api/endpoints/class-wp-rest-users-controller.php
@@ -367,8 +367,10 @@ class WP_REST_Users_Controller extends WP_REST_Controller {
 		if ( $total_users < 1 ) {
 			// Out-of-bounds, run the query again without LIMIT for total count.
 			unset( $prepared_args['number'], $prepared_args['offset'] );
-			$count_query = new WP_User_Query( $prepared_args );
-			$total_users = $count_query->get_total();
+
+			$prepared_args['number'] = 1;
+			$count_query             = new WP_User_Query( $prepared_args );
+			$total_users             = $count_query->get_total();
 		}
 
 		$response->header( 'X-WP-Total', (int) $total_users );
