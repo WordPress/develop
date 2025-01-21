46b534006a85755e30a3c8ff7d693750c5a35856
Jb Audras <audrasjb@gmail.com>
2025-01-21 20:24:51 +0100

    Update admin-functions.php

--- src/wp-admin/admin-functions.php
+++ src/wp-admin/admin-functions.php
@@ -14,7 +14,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die( '-1' );
 }
 
-
 _deprecated_file( basename( __FILE__ ), '2.5.0', 'wp-admin/includes/admin.php' );
 
 /** WordPress Administration API: Includes all Administration functions. */
