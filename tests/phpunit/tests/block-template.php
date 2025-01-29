2dc4d573bb462bfaf4667d6f39f6fdf8b681d341
Lars Gersmann <lars.gersmann@gmail.com>
2025-01-29 08:27:55 +0100

    Ensure contributed plugin template is included in block templates test

--- tests/phpunit/tests/block-template.php
+++ tests/phpunit/tests/block-template.php
@@ -447,6 +447,7 @@ class Tests_Block_Template extends WP_UnitTestCase {
 
 		$templates = get_block_templates();
 
+		# ensure our contributed plugin template is in the list
 		$templates_by_title = array_column( $templates, null, 'title' );
 		$this->assertArrayHasKey( $template_name, $templates_by_title );
 
