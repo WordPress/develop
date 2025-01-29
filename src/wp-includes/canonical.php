147789eee85b722eb869512c40a1476c821c091d
Sukhendu Sekhar Guria <sukhendu.guria@rtcamp.com>
2025-01-29 16:33:38 +0530

    Use is_post_status_viewable for non-public status check

--- src/wp-includes/canonical.php
+++ src/wp-includes/canonical.php
@@ -950,15 +950,22 @@ function redirect_guess_404_permalink() {
 	}
 
 	if ( get_query_var( 'name' ) ) {
-		$exact_match = $wpdb->get_var(
-			$wpdb->prepare(
-				"SELECT ID FROM $wpdb->posts WHERE post_name = %s LIMIT 1",
-				get_query_var( 'name' )
-			)
+		$non_public_statuses = array_diff(
+			get_post_stati(),
+			array_filter( get_post_stati(), 'is_post_status_viewable' )
 		);
 
-		if ( $exact_match ) {
-			return false;
+		if ( ! empty( $non_public_statuses ) ) {
+			$exact_match = $wpdb->get_var(
+				$wpdb->prepare(
+					"SELECT ID FROM $wpdb->posts WHERE post_name = %s AND post_status IN (" . implode( ',', array_fill( 0, count( $non_public_statuses ), '%s' ) ) . ') LIMIT 1',
+					array_merge( array( get_query_var( 'name' ) ), array_values( $non_public_statuses ) )
+				)
+			);
+
+			if ( $exact_match ) {
+				return false;
+			}
 		}
 
 		$publicly_viewable_statuses   = array_filter( get_post_stati(), 'is_post_status_viewable' );
