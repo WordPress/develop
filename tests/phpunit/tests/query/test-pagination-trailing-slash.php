00239b1497967a603125383e37257b62277ec330
Ankit Kumar Shah <ankit.shah@rtcamp.com>
2025-01-31 06:01:04 +0530

    Add with trailing slash test

--- tests/phpunit/tests/query/test-pagination-trailing-slash.php
+++ tests/phpunit/tests/query/test-pagination-trailing-slash.php
@@ -12,6 +12,7 @@ class Tests_Query_Pagination_Trailing_Slash extends WP_UnitTestCase {
 	 * Test that pagination links respect the permalink structure's trailing slash setting.
 	 */
 	public function test_pagination_links_trailing_slash_consistency() {
+		// Test without trailing slash in permalink structure.
 		$this->set_permalink_structure( '/%postname%' );
 
 		$args = array(
@@ -43,6 +44,32 @@ class Tests_Query_Pagination_Trailing_Slash extends WP_UnitTestCase {
 			'class="prev page-numbers" href="http://example.org/category/test"',
 			$links,
 			'Previous link should not have trailing slash when permalink structure has no trailing slash'
+		);
+
+		// Test with trailing slash in permalink structure.
+		$this->set_permalink_structure( '/%postname%/' );
+
+		$links = paginate_links( $args );
+
+		// Test page 1 link (should have trailing slash).
+		$this->assertStringContainsString(
+			'href="http://example.org/category/test/"',
+			$links,
+			'Page 1 link should have trailing slash when permalink structure has trailing slash'
+		);
+
+		// Test page 3 link (should have trailing slash).
+		$this->assertStringContainsString(
+			'href="http://example.org/category/test/page/3/"',
+			$links,
+			'Page 3 link should have trailing slash when permalink structure has trailing slash'
+		);
+
+		// Test previous link (should have trailing slash).
+		$this->assertStringContainsString(
+			'class="prev page-numbers" href="http://example.org/category/test/"',
+			$links,
+			'Previous link should have trailing slash when permalink structure has trailing slash'
 		);
 	}
 }
